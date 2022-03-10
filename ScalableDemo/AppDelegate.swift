133454deb1b7e526d2a4bca5d5369b760efac909
abdulrahim46 <abdul7716@gmail.com>
2022-03-10 22:50:11 +0530

    Add navigation controller for rootview

--- ScalableDemo/AppDelegate.swift
+++ ScalableDemo/AppDelegate.swift
@@ -15,8 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let navController = UINavigationController(rootViewController: ViewController())
         window = UIWindow()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = navController
         window?.makeKeyAndVisible()
         
         return true
--- ScalableDemo/SceneDelegate.swift
+++ ScalableDemo/SceneDelegate.swift
@@ -18,9 +18,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        let navController = UINavigationController(rootViewController: ViewController())
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = navController
         window?.makeKeyAndVisible()
     }
     
