99427b784d6c4b6676b78359999d07de395e2288
abdulrahim46 <abdul7716@gmail.com>
2022-03-10 23:01:05 +0530

    Add network manager

---
+++ ScalableDemo/Network/NetworkManager.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkManager.swift
+//  ScalableDemo
+//
+//  Created by Abdul Rahim on 10/03/22.
+//
+
+import Foundation
+
+class NetworkManager: DataProvider {
+    
+    /// custom error for apis
+    enum CustomError: Error {
+        case invalidUrl
+        case invalidData
+    }
+    
+    /// generic request for all apis
+    func request<T: Codable>(urlName: ServiceURLType, expecting: T.Type, completion: @escaping(Result<T, Error>) -> Void) {
+        
+        let urlString = URLManager.getUrlString(for: urlName)
+        let completeUrl = URL(string: urlString)
+        guard let url = completeUrl else {
+            completion(.failure(CustomError.invalidUrl))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(CustomError.invalidData))
+                }
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(expecting, from: data)
+                completion(.success(result))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+        
+    }
+}
